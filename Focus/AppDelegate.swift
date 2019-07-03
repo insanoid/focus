256a3aa735db04ae7f9cfe0f398742f2cc0b1238
Karthikeya Udupa <karthikeyaudupa@gmail.com>
2019-07-03 17:19:03 +0200

    Initial Commit

---
+++ Focus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Focus
+//
+//  Created by Karthikeya Udupa on 03/07/2019.
+//  Copyright © 2019 Karthikeya Udupa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
